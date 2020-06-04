b8e902b2301899fa962dd82fba5ab3e57b998385
amrchnk <48685431+amrchnk@users.noreply.github.com>
2020-06-04 17:35:40 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,107 @@
+<template>
+  <div id="app">
+    <Lab/>
+  </div>
+</template>
+
+<script>
+
+import Lab from '@/components/lab'
+export default {
+  name:'App',
+  components:{
+    Lab
+  }
+};
+</script>
+
+<style>
+  body{
+      font-size: 15px !important;
+  }
+  
+  button{
+      font-size: 15px !important;
+  }
+  
+  .form {
+      width: 400px;
+      margin: auto auto;
+      margin-top: 50px;
+      border: 0.5px solid #E5E5E5;
+      padding: 20px 25px;
+      border-radius: 10px;
+  
+  }
+  
+  .note{
+    display: flex;
+      flex-direction: column;
+      align-items: center;
+  }
+  
+  .check{
+      margin-top: 15px;
+  }
+  
+  .note-in{
+    margin-top: 30px;
+      float: left;
+  }
+  
+  .features{
+      float: right;
+      display: flex;
+  }
+  
+  .field {
+      margin-top: 5px;
+      clear:both; 
+      text-align:right;
+  }
+  
+  label {
+      float:left;
+      padding-right:10px;
+  }
+  
+   p input[type='checkbox'] {
+   margin-right: 10px;
+  }
+  .best{
+    margin-left: 15px;
+  }
+  
+  input {
+      margin-left: -15px;
+  }
+  
+  span{
+      color: yellow;
+      margin-left: 10px;
+  }
+  
+  
+  
+  li {
+      list-style-type: none;
+      display: flex;
+  }
+  
+  .delete {
+      cursor: pointer;
+      text-decoration: none;
+      display: block;
+      width: 20px;
+      height: 20px;
+      margin-left: 10px;
+  }
+  
+  .delete:hover {
+      transition: all 0.5s;
+  }
+  
+  .radio{
+    margin-top: 15px;
+  }
+</style>
---
+++ src/components/lab.vue
@@ -0,0 +1,157 @@
+<template>
+		<div>
+			<div class="form">
+				<div class="field">
+					<label>Фамилия</label>
+					<input type="text" v-model="elem.surname">
+				</div>
+	
+					<div class="field">
+						<label>Имя</label>
+						<input type="text" v-model="elem.name">
+					</div>
+	
+					<div class="field">
+						<label>Отчество</label>
+						<input type="text" v-model="elem.otch">	
+					</div>
+							
+					<div class="field">
+						<label>Телефон</label>
+						<input type="text" v-model="elem.phone">
+					</div>
+						
+			
+						<p class="check"><input type="checkbox" v-model="elem.best">Избранное</p>
+							
+						<button class="btn btn-outline-primary" v-on:click="Add()">Добавить</button>
+							
+						<div class="radio">
+							<div class="form-check form-check-inline">
+								<input class="form-check-input" value="surname" type="radio" v-model="sort" @click="Sort('surname')"><p class="form-check-label">Сортировать по фамилии</p>
+							</div>
+					
+							<div class="form-check form-check-inline">
+								<input class="form-check-input" value="name" type="radio" v-model="sort"  @click="Sort('name')"><p class="form-check-label">Сортировать по имени</p>
+							</div>
+						</div>
+			</div>
+		
+		
+			<div class="note">
+				<div class="note-in">
+					<ul>
+						<li class="field" v-for="(item,id) in bests" v-bind:key=id>
+								<label>{{item.surname}} {{item.name}} {{item.otch}} {{item.phone}}</label>
+								<div class="features">
+									<span>★</span>
+									<img src="../assets/buck.png" class="delete" href="#" @click="Delete('best',id)" onmouseover="this.src='../assets/buck_hover.png'" onmouseout="this.src='../assets/buck.png'">
+								</div>
+								
+						</li>
+		
+							<li class="field" v-for="(item,id) in contacts" v-bind:key=id>
+								<label>{{item.surname}} {{item.name}} {{item.otch}} {{item.phone}} </label>
+								<div class="features">
+									<img src="../assets/buck.png" class="delete" href="#" @click="Delete('contact',id)" onmouseover="this.src='../assets/buck_hover.png'" onmouseout="this.src='../assets/buck.png'">
+								</div>
+								
+							</li>
+						</ul>
+					</div>
+					
+			</div>
+		</div>
+</template>
+
+<script>
+	export default{
+		data(){
+			return{
+				elem:{
+				surname:"",
+				name:"",
+				otch:"",
+				phone:"",
+				best:false
+				},
+				sort:"surname",
+				contacts:[],
+				bests:[]
+			}
+			
+		},
+		methods:{
+			Add: function(){
+				if(!(this.elem.surname==""||this.elem.name==""||this.elem.otch==""||this.elem.phone=="")){
+					if((this.contacts.find(x => x.phone === this.elem.phone)===undefined)&&(this.bests.find(x => x.phone === this.elem.phone)===undefined)){
+						if(!this.elem.best){
+							this.contacts.push({
+								surname:this.elem.surname,
+								name:this.elem.name,
+								otch:this.elem.otch,
+								phone:this.elem.phone,
+								best:this.elem.best
+							});
+						}
+
+						else{
+							this.bests.push({
+								surname:this.elem.surname,
+								name:this.elem.name,
+								otch:this.elem.otch,
+								phone:this.elem.phone,
+								best:this.elem.best
+							});
+						}
+
+						this.elem.surname="";
+						this.elem.name="";
+						this.elem.otch="";
+						this.elem.phone="";
+						this.elem.best="";
+
+						if(this.sort==="surname"){
+							this.contacts.sort((a, b) => a.surname > b.surname ? 1 : -1);
+							this.bests.sort((a, b) => a.surname > b.surname ? 1 : -1);
+						}
+
+						if(this.sort==="name"){
+							this.contacts.sort((a, b) => a.name > b.name ? 1 : -1);
+							this.bests.sort((a, b) => a.name > b.name ? 1 : -1);
+						}
+					}
+					
+					else{
+						alert("Такой номер уже существует!");
+					}
+				}
+				
+				else{
+					alert("Поля не должны быть пустыми!");
+				}
+			},
+
+		Sort:function(param){
+			if(param=="surname"){
+				this.contacts.sort((a, b) => a.surname > b.surname ? 1 : -1);
+				this.bests.sort((a, b) => a.surname > b.surname ? 1 : -1);
+			}
+			else if(param=="name"){
+				this.contacts.sort((a, b) => a.name > b.name ? 1 : -1);
+				this.bests.sort((a, b) => a.name > b.name ? 1 : -1);
+			}
+			
+		},
+
+		Delete: function(item,id){
+			if(item=='best')this.bests.splice(id, 1);
+			else if (item=='contact')this.contacts.splice(id, 1);
+			
+		}
+		},
+		components:{
+
+		}
+	}
+</script>
